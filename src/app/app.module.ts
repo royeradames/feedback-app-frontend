98e03e98e09013a897d22789d2c948c0380d4bc2
royeradames <royeraadames@gmail.com>
2021-12-28 18:37:17 -0500

    📦declare custom drop down and new feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewFeedbackComponent } from './new-feedback/new-feedback.component';
+import { CustomDropDownComponent } from './custom-drop-down/custom-drop-down.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NewFeedbackComponent, CustomDropDownComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
