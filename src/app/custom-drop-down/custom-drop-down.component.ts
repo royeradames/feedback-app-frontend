d3dec97a6d19f1606191dec7278758de0b453a58
royeradames <royeraadames@gmail.com>
2021-12-28 18:34:43 -0500

    📦handle toggle of downdown options list when field is click

---
+++ src/app/custom-drop-down/custom-drop-down.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-drop-down',
+  templateUrl: './custom-drop-down.component.html',
+  styleUrls: ['./custom-drop-down.component.scss'],
+})
+export class CustomDropDownComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  isHidden: boolean = true;
+}
